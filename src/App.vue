db7e74c12639a240bd0dccc8948bfb20ecc4e8a1
Caio <caiomarianni@gmail.com>
2024-09-04 23:56:10 -0300

    basic changes

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,8 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <header>
+  <header class="m-0 top-0 left-0">
+    <img src="./assets/coner.png" alt="">
     <p>something</p>
   </header>
 
