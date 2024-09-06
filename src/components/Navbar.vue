fd266124effc991f1102f3b1429deef592009858
Caio <caiomarianni@gmail.com>
2024-09-06 02:08:12 -0300

    navbar details

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,13 +2,19 @@
 import { RouterLink } from 'vue-router';
 </script>
 <template>
-  <div class="relative font-terFont overflow-hidden md:mt-8 py-4 mt-8 px-5 rounded-[20px] border-black border-2 mb-2">
-    <h1 class="text-5xl">Home<span class="text-gray text-2xl font-medium">/</span></h1>
-    <ul class="flex gap-8 text-gray-400">
-      <li class="hover:text-black transition-all duration-150"><RouterLink to="">programing</RouterLink></li>
-      <li class="hover:text-black transition-all duration-150"><RouterLink to="">designer</RouterLink></li>
-      <li class="hover:text-black transition-all duration-150"><RouterLink to="">dj</RouterLink></li>
-    </ul>
-    <img class="-z-10 absolute w-32 top-0 -translate-x-5 -translate-y-5" src="../assets/coner.png" alt="">
+  <div class="flex items-center w-full mt-6 rounded-[20px] overflow-hidden">
+    <!-- Navigation -->
+    <div class="relative w-full bg-black h-[110px] py-5 pl-5 rounded-l-[20px] border-l-2 border-t-2 border-b-2 font-terFont">
+      <h1 class="absolute z-10 text-5xl">Home<span class="text-gray text-2xl font-medium">/</span></h1>
+      <ul class="absolute bottom-4 flex gap-8 text-gray-400">
+        <li class="hover:text-black transition-all duration-150"><RouterLink to="">programing</RouterLink></li>
+        <li class="hover:text-black transition-all duration-150"><RouterLink to="">designer</RouterLink></li>
+        <li class="hover:text-black transition-all duration-150"><RouterLink to="">dj</RouterLink></li>
+      </ul>
+      <!-- Border Detail -->
+      <img class="absolute w-32 top-0 -translate-x-5 -translate-y-5" src="../assets/coner.png" alt="">
+    </div>
+    <!-- Final Detail -->
+    <img class="h-[110px] w-[300px]" src="../assets/teste1.png" alt="">
   </div>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,15 @@
 </script>
 
 <template>
+  <div class="flex flex-col justify-center items-center">
+    <h1 class="my-5 text-6xl font-terFont">Criativity</h1>
+    <!-- Icons -->
+     <div class="flex justify-evenly flex-wrap gap-3">
+      <div class="">tste</div>
+      <div class="">tste</div>
+      <div class="">tste</div>
+     </div>
+  </div>
   <div class="h-[2000px]"></div>
   teste da hoem
 </template>
